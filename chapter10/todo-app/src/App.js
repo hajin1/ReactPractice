459caa373cd6aa78b3b43f0441b1ea95315b26b0
hajin <hajin@synapsoft.co.kr>
2020-03-11 08:22:20 +0900

    10.3 기능구현하기 - 생성, 삭제, 체크박스 토글 구현

--- chapter10/todo-app/src/App.js
+++ chapter10/todo-app/src/App.js
@@ -1,15 +1,62 @@
-import React from "react";
+import React, {useState, useRef, useCallback} from "react";
 import TodoTemplate from "./component/TodoTemplate";
 import TodoInsert from "./component/TodoInsert";
 import TodoList from "./component/TodoList";
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '자바스크립트',
+      checked: false,
+    },
+    {
+      id: 3,
+      text: 'css 공부',
+      checked: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
+  // 인자로 입력된 id를 지움
+  const onRemove = useCallback( id => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }, [todos]);
+
+  const onToggle = useCallback(id => {
+    setTodos(
+      todos.map(todo => 
+        todo.id === id? {...todo, checked : !todo.checked } : todo),
+    );
+  });
+
   return (
     <TodoTemplate>
-      <TodoInsert />
-      <TodoList />
+      <TodoInsert onInsert={onInsert}/>
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
     </TodoTemplate>
   );
 };
+
+
 
 export default App;
