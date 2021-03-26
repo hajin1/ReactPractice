a91ed5dab4fefdad6be506116bfed4fbb0e8fca7
hajin <hajin@synapsoft.co.kr>
2021-03-26 21:17:10 +0900

    hello-react 프로젝트 새로 생성

---
+++ hello-react/src/TodoList.js
@@ -0,0 +1,31 @@
+import React, { useCallback } from 'react';
+import TodoListItem from './TodoListItem';
+import './TodoList.scss';
+import { List } from 'react-virtualized';
+
+const TodoList = ({ todos, onRemove, onToggle }) => {
+    const rowRenderer = useCallback(
+        ({ index, key, style }) => {
+            const todo = todos[index];
+            return (
+                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} />
+            );
+        },
+        [onRemove, onToggle, todos]
+    );
+
+    return (
+        <List
+            className="TodoList"
+            width={512}
+            height={513}
+            rowCount={todos.length}
+            rowHeight={57}
+            rowRenderer={rowRenderer}
+            list={todos}
+            style={{ outline: 'none' }}
+        />
+    )
+}
+
+export default React.memo(TodoList);
