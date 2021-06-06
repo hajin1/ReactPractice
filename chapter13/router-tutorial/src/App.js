3b7b898da5a2ea07c1b0485931342672f97888e5
hajin <hajin@synapsoft.co.kr>
2021-06-06 22:03:20 +0900

    chapter 13.2 프로젝트에 라우터 적용 - Link 컴포넌트를 상요하영 다른 주소로 이동하기

--- chapter13/router-tutorial/src/App.js
+++ chapter13/router-tutorial/src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import { Route, Link } from 'react-router-dom';
+import About from './About';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      Hello World!
+
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route path="/" component={Home} exact={true} />
+      <Route path="/about" component={About} />
     </div>
   );
 }
