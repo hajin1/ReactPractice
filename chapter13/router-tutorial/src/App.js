227d2aae60be9ebcebc1ded06732036e9bc36159
hajin <hajin@synapsoft.co.kr>
2021-06-06 22:43:04 +0900

    chapter 13.5 서브라우트 - 라우트 내부에 또 라우트를 정의하는 것

--- chapter13/router-tutorial/src/App.js
+++ chapter13/router-tutorial/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { Route, Link } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
-import Profile from './Profile';
+import Profiles from './Profiles';
 
 function App() {
   return (
@@ -16,16 +16,13 @@ function App() {
           <Link to="/about">소개</Link>
         </li>
         <li>
-          <Link to="/profile/misaeng">미생이 프로필</Link>
-        </li>
-        <li>
-          <Link to="/profile/gildong">길동이 프로필</Link>
+          <Link to="/profiles">프로필</Link>
         </li>
       </ul>
       <hr />
       <Route path="/" component={Home} exact={true} />
       <Route path={["/about", "/info"]} component={About} />
-      <Route path="/profile/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
     </div>
   );
 }
